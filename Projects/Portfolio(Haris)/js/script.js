be712f139067a469d86003b643a5bec52801a564
Haris Imtiaz <haris.imtiaz.skipq@gmail.com>
2024-04-17 22:11:27 +0500

    Add files via upload

---
+++ Projects/Portfolio(Haris)/js/script.js
@@ -0,0 +1,21 @@
+var typed = new Typed(".typing",{
+    // strings:["","Web Developer","Web Designer","Graphic Designer","Freelancer"],
+    strings:["","Web Developer","Web Designer","React Js Developer","Next Js Developer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
+
+
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length;
+    for(let i=0; i<totalNavList; i++){
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click", function(){
+            for(let j=0; j<totalNavList; j++){
+                navList[j].querySelector("a").classList.remove("active");
+            }
+            this.classList.add("active")
+        })
+    }
